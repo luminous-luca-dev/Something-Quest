da4c911f124af060a5a50cf14ab6286a9b3cdebd
Lumitec <taigo.hs.2007@gmail.com>
2026-03-12 05:07:37 +0000

    main.cppを追加し、基本的なユーザー入力と日本語の出力を実装

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string username;
+    cout << "Hello World." << endl;
+    cout << "Welcome to my core." << endl;
+    cout << "ブレイン内思考ロジック から 日本語話者 と推定。" << endl;
+    cout << "出力言語を 日本語 に変換" << endl;
+    cout << "あなたのお名前は？" << endl;
+    cin >> username;
+    cout << username << "... 良い名前ですね。" << endl;
+    return 0;
+}
